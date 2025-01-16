7a3699e3f805920bf2c420da1801f239d4a95c80
Leonardo <lbruni10@gmail.com>
2025-01-16 15:50:03 -0300

    Implementing web api project example to start building project services + Updated gitignore for a global settings

---
+++ src/posterr-services/Posterr.Domain/Entities/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posterr.Domain.Entities
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Description { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
---
+++ src/posterr-services/Posterr.Infrastructure/Contexts/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Posterr.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posterr.Infrastructure.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
